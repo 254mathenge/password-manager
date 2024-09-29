c247a416e74fb33fd441166e4800a7fd6c092afe
254mathenge <mathengefaith49@gmail.com>
2024-09-29 15:48:51 +0300

    adds view function

--- password.py
+++ password.py
@@ -1,15 +1,31 @@
 import random
-password= input("Input master password: ")
+
+password = input("Input master password: ")
+
+
 def add():
-    name=input("Enter account Name: ")
-    pwd =input("Enter Password: ")
-    with open("passwords.txt","a") as f:
-     f.write(name+"|"+"pwd")   
-        
+    name = input("Enter account Name: ")
+    pwd = input("Enter Password: ")
+    with open("passwords.txt", "a") as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+def view():
+    with open("passwords.txt","r") as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
 if password == "faith":
-        print("welcome Faith!!")
+    print("welcome Faith!!")
+    view_add = input("Would you like to add a new password or view existing ones(view,add),press q to quit? ")
+    if view_add == "add":
         add()
-        
-else:
-        print("invalid")
+    elif view_add == "view":
+        view()
+    elif view_add == "q":
         quit()
+    else:
+        print("Invalid input")
+else:
+    print("invalid")
+    quit()
